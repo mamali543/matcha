727fa59cf01c880dae8a86c918e325d5c27b8c4e
mamali543 <amalyreda@gmail.com>
2024-02-16 20:03:15 +0100

    home desigh done..

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/domain/user.model';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
   users: User[] = [new User(1, "Elias", "70m", [ '/assets/lkher.jpeg', '/assets/image1.jpeg', '/assets/saad.jpeg' ])];
+
+  currentImageIndex: number = 0;
+  imageChangeSubscription!: Subscription;
+
+  ngOnInit(): void {
+    const imageChangeInterval = interval(6000);
+    this.imageChangeSubscription = imageChangeInterval.subscribe(() => {
+        this.currentImageIndex = (this.currentImageIndex + 1) % this.users[0].images.length;
+    })
+  }
 
 }
 
