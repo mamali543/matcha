dbddf743daabda7515faa2f35eb26c3f46b7a4eb
mamali543 <amalyreda@gmail.com>
2024-03-11 14:58:12 +0000

    matcha auth

--- service/run.py
+++ service/run.py
@@ -1,13 +1,5 @@
 from app import create_app
 
-
-
-# @app.route('/signup', methods=['POST', 'OPTIONS'])
-# @cross_origin(origins="http://localhost:4200")
-# def test():
-#      print(request.json)
-#      return jsonify({"message": "User registered successfully"}), 201
-
 app = create_app()
 
 if __name__ == '__main__':
