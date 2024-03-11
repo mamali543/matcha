83f523561138bd67f7f5a5a57cb7de62d810cfec
mamali543 <amalyreda@gmail.com>
2024-03-11 17:47:05 +0000

    matcha auth

--- service/run.py
+++ service/run.py
@@ -1,3 +1,12 @@
+#source myvenv/bin/activate this means that any python commands u'll run will use the packages installed in the virtual envirenement
+
+#the init.py file in each folder serves as an indicator to python that the directory should be treated as a python package
+#it can also contain initialization code that will be executed when the package is imported
+
+#mysql server is the core component that manages the database, processes sql queries and handles client connections, it's responsible for data persistence
+#nysql client is a tool or software that's used to interact and connect to the mysql server
+
+
 from app import create_app
 
 app = create_app()
